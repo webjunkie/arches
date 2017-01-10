1a7f3eb5690cf5b8c622947cfe4ec9e6237b28b0
Alexei Peters <apeters@fargeo.com>
2017-01-09 17:53:07 -0800

    fix general page setup issues, re #1337

--- arches/app/media/js/views/importexport.js
+++ arches/app/media/js/views/importexport.js
@@ -3,41 +3,43 @@ require([
     'knockout',
     'underscore',
     'arches',
-    'views/base-manager'
+    'views/base-manager',
+    'dropzone',
+    'uuid',
+    'bindings/dropzone'
 ], function($, ko, _, arches, BaseManagerView) {
     var ImportExportView = BaseManagerView.extend({
-       initialize: function(options) {
-           BaseManagerView.prototype.initialize.call(this, options);
+        initialize: function(options) {
+            this.viewModel.showFind = ko.observable(false);
+            this.viewModel.dropzoneOptions = {
+                url: "/",
+                dictDefaultMessage: '',
+                autoProcessQueue: false,
+                previewTemplate: $("template#file-widget-dz-preview").html(),
+                autoQueue: false,
+                // previewsContainer: ".dz-previews." + this.uniqueidClass(),
+                // clickable: ".fileinput-button." + this.uniqueidClass(),
+                // acceptedFiles: this.acceptedFiles(),
+                // maxFilesize: this.maxFilesize(),
+                init: function() {
+                    self.dropzone = this;
+
+                    this.on("addedfile", function(file) {
+                        self.filesForUpload.push(file);
+                    });
+
+                    this.on("error", function(file, error) {
+                        file.error = error;
+                        self.filesForUpload.valueHasMutated()
+                    });
+
+                    this.on("removedfile", function(file) {
+                        self.filesForUpload.remove(file);
+                    });
+                }
+            };
+            BaseManagerView.prototype.initialize.call(this, options);
        }
    });
    return new ImportExportView();
-    // $(document).ready(function() {
-        // this.dropzoneOptions = {
-        //     url: "/",
-        //     dictDefaultMessage: '',
-        //     autoProcessQueue: false,
-        //     previewTemplate: $("template#file-widget-dz-preview").html(),
-        //     autoQueue: false,
-        //     previewsContainer: ".dz-previews." + this.uniqueidClass(),
-        //     clickable: ".fileinput-button." + this.uniqueidClass(),
-        //     acceptedFiles: this.acceptedFiles(),
-        //     maxFilesize: this.maxFilesize(),
-        //     init: function() {
-        //         self.dropzone = this;
-        //
-        //         this.on("addedfile", function(file) {
-        //             self.filesForUpload.push(file);
-        //         });
-        //
-        //         this.on("error", function(file, error) {
-        //             file.error = error;
-        //             self.filesForUpload.valueHasMutated()
-        //         });
-        //
-        //         this.on("removedfile", function(file) {
-        //             self.filesForUpload.remove(file);
-        //         });
-        //     }
-        // };
-    //});
 });
